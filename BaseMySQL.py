96b49a598fdbeca2f18d25a0fb7e1c5830c91f26
Siggi Bjarnason <siggi.bjarnason@t-mobile.com>
2020-11-16 23:10:07 +0000

    BaseMySQL

---
+++ BaseMySQL.py
@@ -0,0 +1,230 @@
+'''
+Script to deploy new database table view to VMInfo web server
+Author Siggi Bjarnason Copyright 2020
+
+Following packages need to be installed as administrator
+pip install pymysql
+
+'''
+# Import libraries
+import sys
+import os
+import time
+import platform
+import pymysql
+# End imports
+
+ISO = time.strftime("-%Y-%m-%d-%H-%M-%S")
+
+def CleanExit(strCause):
+  objLogOut.close()
+  print ("objLogOut closed")
+  sys.exit(9)
+
+def LogEntry(strMsg,bAbort=False):
+  strTimeStamp = time.strftime("%m-%d-%Y %H:%M:%S")
+  objLogOut.write("{0} : {1}\n".format(strTimeStamp,strMsg))
+  print (strMsg)
+  if bAbort:
+    CleanExit("")
+
+def processConf():
+  global strServer
+  global strDBUser
+  global strDBPWD
+  global strInitialDB
+
+
+  LogEntry ("Looking for configuration file: {}".format(strConf_File))
+  if os.path.isfile(strConf_File):
+    LogEntry ("Configuration File exists")
+  else:
+    LogEntry ("Can't find configuration file {}, make sure it is the same directory as this script".format(strConf_File))
+    LogEntry("{} on {}: Exiting.".format (strScriptName,strScriptHost))
+    objLogOut.close()
+    sys.exit(9)
+
+  strLine = "  "
+  LogEntry ("Reading in configuration")
+  objINIFile = open(strConf_File,"r")
+  strLines = objINIFile.readlines()
+  objINIFile.close()
+
+  for strLine in strLines:
+    strLine = strLine.strip()
+    iCommentLoc = strLine.find("#")
+    if iCommentLoc > -1:
+      strLine = strLine[:iCommentLoc].strip()
+    else:
+      strLine = strLine.strip()
+    if "=" in strLine:
+      strConfParts = strLine.split("=")
+      strVarName = strConfParts[0].strip()
+      strValue = strConfParts[1].strip()
+      strFullValue = strConfParts[1].strip()
+      if strVarName == "Server":
+         strServer = strValue
+      if strVarName == "dbUser":
+        strDBUser = strValue
+      if strVarName == "dbPWD":
+        strDBPWD = strFullValue
+      if strVarName == "InitialDB":
+        strInitialDB = strValue
+
+  LogEntry ("Done processing configuration, moving on")
+
+def isInt (CheckValue):
+  # function to safely check if a value can be interpreded as an int
+  if isinstance(CheckValue,int):
+    return True
+  elif isinstance(CheckValue,str):
+    if CheckValue.isnumeric():
+      return True
+    else:
+      return False
+  else:
+    return False
+
+def ConvertFloat (fValue):
+  if isinstance(fValue,(float,int,str)):
+    try:
+      fTemp = float(fValue)
+    except ValueError:
+      fTemp = "NULL"
+  else:
+    fTemp = "NULL"
+  return fTemp
+
+def DBClean(strText):
+  if strText is None:
+    return ""
+  strTemp = str(strText)
+  strTemp = strTemp.encode("ascii","ignore")
+  strTemp = strTemp.decode("ascii","ignore")
+  strTemp = strTemp.replace("\\","\\\\")
+  strTemp = strTemp.replace("'","\"")
+  return strTemp
+
+def SQLConn (strServer,strDBUser,strDBPWD,strInitialDB):
+  try:
+    # Open database connection
+    return pymysql.connect(strServer,strDBUser,strDBPWD,strInitialDB)
+  except pymysql.err.InternalError as err:
+    print ("Error: unable to connect: {}".format(err))
+    sys.exit(5)
+  except pymysql.err.OperationalError as err:
+    print ("Operational Error: unable to connect: {}".format(err))
+    sys.exit(5)
+  except pymysql.err.ProgrammingError as err:
+    print ("Programing Error: unable to connect: {}".format(err))
+    sys.exit(5)
+
+def SQLQuery (strSQL,db):
+  try:
+    # prepare a cursor object using cursor() method
+    dbCursor = db.cursor()
+    # Execute the SQL command
+    dbCursor.execute(strSQL)
+    # Count rows
+    iRowCount = dbCursor.rowcount
+    if strSQL[:6].lower() == "select" or strSQL[:4].lower() == "call":
+      dbResults = dbCursor.fetchall()
+    else:
+      db.commit()
+      dbResults = ()
+    return [iRowCount,dbResults]
+  except pymysql.err.InternalError as err:
+    if strSQL[:6].lower() != "select":
+      db.rollback()
+    return "Internal Error: unable to execute: {}\n{}".format(err,strSQL)
+  except pymysql.err.ProgrammingError as err:
+    if strSQL[:6].lower() != "select":
+      db.rollback()
+    return "Programing Error: unable to execute: {}\n{}".format(err,strSQL)
+  except pymysql.err.OperationalError as err:
+    if strSQL[:6].lower() != "select":
+      db.rollback()
+    return "Programing Error: unable to execute: {}\n{}".format(err,strSQL)
+  except pymysql.err.IntegrityError as err:
+    if strSQL[:6].lower() != "select":
+      db.rollback()
+    return "Integrity Error: unable to execute: {}\n{}".format(err,strSQL)
+  except pymysql.err.DataError as err:
+    if strSQL[:6].lower() != "select":
+      db.rollback()
+    return "Data Error: unable to execute: {}\n{}".format(err,strSQL)
+
+def ValidReturn(lsttest):
+  if isinstance(lsttest,list):
+    if len(lsttest) == 2:
+      if isinstance(lsttest[0],int) and isinstance(lsttest[1],tuple):
+        return True
+      else:
+        return False
+    else:
+      return False
+  else:
+    return False
+
+def main():
+  global objLogOut
+  global strConf_File
+  global strScriptName
+  global strScriptHost
+
+  strBaseDir = os.path.dirname(sys.argv[0])
+  strRealPath = os.path.realpath(sys.argv[0])
+  strRealPath = strRealPath.replace("\\","/")
+  if strBaseDir == "":
+    iLoc = strRealPath.rfind("/")
+    strBaseDir = strRealPath[:iLoc]
+  if strBaseDir[-1:] != "/":
+    strBaseDir += "/"
+  strLogDir  = strBaseDir + "Logs/"
+  if strLogDir[-1:] != "/":
+    strLogDir += "/"
+  iLoc = sys.argv[0].rfind(".")
+  strConf_File = sys.argv[0][:iLoc] + ".ini"
+
+  if not os.path.exists (strLogDir) :
+    os.makedirs(strLogDir)
+    print ("\nPath '{0}' for log files didn't exists, so I create it!\n".format(strLogDir))
+
+  strScriptName = os.path.basename(sys.argv[0])
+  iLoc = strScriptName.rfind(".")
+  strLogFile = strLogDir + strScriptName[:iLoc] + ISO + ".log"
+  strVersion = "{0}.{1}.{2}".format(sys.version_info[0],sys.version_info[1],sys.version_info[2])
+  strScriptHost = platform.node().upper()
+
+
+  print ("This is a script to create a new dynamic database table on VMInfo. " 
+          " This is running under Python Version {}".format(strVersion))
+  print ("Running from: {}".format(strRealPath))
+  dtNow = time.asctime()
+  print ("The time now is {}".format(dtNow))
+  print ("Logs saved to {}".format(strLogFile))
+  objLogOut = open(strLogFile,"w",1)
+
+  processConf()
+  dbConn = ""
+  dbConn = SQLConn (strServer,strDBUser,strDBPWD,strInitialDB)
+
+  strSQL = "SELECT * FROM vulnmgmt.tblmbenvironments;"
+  lstReturn = SQLQuery (strSQL,dbConn)
+  if not ValidReturn(lstReturn):
+    LogEntry ("Unexpected: {}".format(lstReturn))
+    CleanExit("due to unexpected SQL return, please check the logs")
+  elif lstReturn[0] == 0:
+    LogEntry ("No data returned")
+  else:
+    strLocation = lstReturn[1][4][1]
+    LogEntry ("Location: {}".format(strLocation))
+
+
+  dtNow = time.asctime()
+  LogEntry ("Completed at {}".format(dtNow))
+  objLogOut.close()
+
+if __name__ == '__main__':
+    main()
+
