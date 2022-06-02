16685369881a04851d286c508a516f9670e9d685
Siggi@Reba <siggi@supergeek.us>
2022-06-02 22:41:40 +0000

    WIP

--- emailtest.py
+++ emailtest.py
@@ -20,27 +20,49 @@ def CleanExit(strCause):
   print (strCause)
   sys.exit(9)
 
+def LogEntry(strmsg):
+  print(strmsg)
+
 def main():
-  if os.getenv("APIKEY") != "" and os.getenv("APIKEY") is not None:
-    strAPIKey = os.getenv("APIKEY")
+  strPort = 465
+  bUseTLS = False
+  if os.getenv("EMAILUSER") != "" and os.getenv("EMAILUSER") is not None:
+    strUser = os.getenv("EMAILUSER")
   else:
-    CleanExit("No API key provided")
+    CleanExit("No email user name provided")
 
+  if os.getenv("EMAILPWD") != "" and os.getenv("EMAILPWD") is not None:
+    strPWD = os.getenv("EMAILPWD")
+  else:
+    CleanExit("No email user password provided")
 
-  sender = 'from@fromdomain.com'
-  receivers = ['to@todomain.com']
+  if os.getenv("EMAILSERVER") != "" and os.getenv("EMAILSERVER") is not None:
+    strServer = os.getenv("EMAILSERVER")
+  else:
+    CleanExit("No email server provided")
 
-  message = """From: From Person <from@fromdomain.com>
-  To: To Person <to@todomain.com>
-  Subject: SMTP e-mail test
+  if os.getenv("EMAILPORT") != "" and os.getenv("EMAILPORT") is not None:
+    strPort = os.getenv("EMAILPORT")
+  else:
+    LogEntry("No server port provided, using the default of {}".format(strPort))
 
-  This is a test e-mail message.
-  """
+  strSender = "Private Person <from@example.com>"
+  strRCTP = "A Test User <to@example.com>"
+
+  strMsg = f"""\
+  Subject: Howdy Partner
+  To: {strRCTP}
+  From: {strSender}
+
+  This is a test e-mail message. just more testing"""
 
   try:
-    smtpObj = smtplib.SMTP('localhost')
-    smtpObj.sendmail(sender, receivers, message)
-    print ("Successfully sent email")
+    objSMTP = smtplib.SMTP(strServer,strPort)
+    if bUseTLS:
+      objSMTP.starttls()
+    objSMTP.login(strUser,strPWD)
+    objSMTP.sendmail(strSender, strRCTP, strMsg)
+    print ("Successfully sent email via {} port {} to {}".format(strServer,strPort,strRCTP))
   except smtplib.SMTPException:
     print ("Error: unable to send email")
 
