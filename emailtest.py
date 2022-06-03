fe70bf19d47e4d731b0ec8e35788fee82d63cb46
Siggi@Reba <siggi@supergeek.us>
2022-06-03 00:17:12 +0000

    both text and html

--- emailtest.py
+++ emailtest.py
@@ -9,10 +9,14 @@ pip install jason
 
 '''
 # Import libraries
-import smtplib
 import sys
 import os
 import smtplib
+import email.message
+import email.policy
+import email.utils
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # End imports
 strPort = 465
@@ -50,8 +54,25 @@ def main():
 
   strSender = "Private Person <from@example.com>"
   strRCTP = "A Test User <to@example.com>"
+  strSubject = "An HTML test email"
+  strHTML = "<p>This is an e-mail message to be sent in HTML format</p>\n<b>This is HTML message.</b>\n<h1>This is headline.</h1>"
+  strTxtBody = "Why aren't you using HTML"
 
-  strMsg = "Subject: Howdy Partner\nTo: {}\nFrom: {}\nThis is a test e-mail message. just more testing".format(strRCTP,strSender)
+  #objMsg = email.message.EmailMessage(email.policy.SMTP)
+  objMsg = MIMEMultipart('alternative')
+  objMsg["To"] = strRCTP
+  objMsg["From"] = strSender
+  objMsg["Subject"] = strSubject
+  objMsg['Date'] = email.utils.formatdate(localtime=True)
+  objMsg['Message-ID'] = email.utils.make_msgid()
+  #objMsg.add_header('Content-Type', 'text/html')
+  #objMsg.set_content(strHTML, subtype="html")
+  #objMsg.set_content(strTxtBody, subtype="plain")
+  #strMsg = "Subject: Howdy Partner\nTo: {}\nFrom: {}\nThis is a test e-mail message. just more testing".format(strRCTP,strSender)
+  oPart1 = MIMEText(strTxtBody, "plain")
+  opart2 = MIMEText(strHTML, "html")
+  objMsg.attach(oPart1)
+  objMsg.attach(opart2)
 
   try:
     objSMTP = smtplib.SMTP(strServer,strPort)
@@ -60,7 +81,8 @@ def main():
       objSMTP.starttls()
     objResponse = objSMTP.login(strUser,strPWD)
     print ("Response from login: {}".format(objResponse))
-    objResponse = objSMTP.sendmail(strSender, strRCTP, strMsg)
+    objResponse = objSMTP.send_message(objMsg)
+    objSMTP.quit()
     print("Response from sendmail: {}".format(objResponse))
     print ("Successfully sent email via {} port {} to {}".format(strServer,strPort,strRCTP))
   except smtplib.SMTPException:
