38ecd44c5d20d653efee189978f8cd5139187f93
Siggi@Reba <siggi@supergeek.us>
2022-06-03 00:29:33 +0000

    modularize emailtest

--- emailtest.py
+++ emailtest.py
@@ -17,11 +17,25 @@ import email.policy
 import email.utils
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from bs4 import BeautifulSoup
 
 # End imports
 strPort = 465
 bUseTLS = False
 iDebugLevel = 0
+
+
+def remove_tags(html):
+
+    # parse html content
+    soup = BeautifulSoup(html, "html.parser")
+
+    for data in soup(['style', 'script']):
+        # Remove tags
+        data.decompose()
+
+    # return data by retrieving the tag content
+    return ' '.join(soup.stripped_strings)
 
 def CleanExit(strCause):
   print (strCause)
@@ -30,7 +44,8 @@ def CleanExit(strCause):
 def LogEntry(strmsg):
   print(strmsg)
 
-def main():
+
+def SendHTMLEmail(strSubject, strBody, strTo, strFrom):
 
   if os.getenv("EMAILUSER") != "" and os.getenv("EMAILUSER") is not None:
     strUser = os.getenv("EMAILUSER")
@@ -52,16 +67,10 @@ def main():
   else:
     LogEntry("No server port provided, using the default of {}".format(strPort))
 
-  strSender = "Private Person <from@example.com>"
-  strRCTP = "A Test User <to@example.com>"
-  strSubject = "An HTML test email"
-  strHTML = "<p>This is an e-mail message to be sent in HTML format</p>\n<b>This is HTML message.</b>\n<h1>This is headline.</h1>"
-  strTxtBody = "Why aren't you using HTML"
-
   #objMsg = email.message.EmailMessage(email.policy.SMTP)
   objMsg = MIMEMultipart('alternative')
-  objMsg["To"] = strRCTP
-  objMsg["From"] = strSender
+  objMsg["To"] = strTo
+  objMsg["From"] = strFrom
   objMsg["Subject"] = strSubject
   objMsg['Date'] = email.utils.formatdate(localtime=True)
   objMsg['Message-ID'] = email.utils.make_msgid()
@@ -69,8 +78,8 @@ def main():
   #objMsg.set_content(strHTML, subtype="html")
   #objMsg.set_content(strTxtBody, subtype="plain")
   #strMsg = "Subject: Howdy Partner\nTo: {}\nFrom: {}\nThis is a test e-mail message. just more testing".format(strRCTP,strSender)
-  oPart1 = MIMEText(strTxtBody, "plain")
-  opart2 = MIMEText(strHTML, "html")
+  oPart1 = MIMEText(remove_tags(strBody), "plain")
+  opart2 = MIMEText(strBody, "html")
   objMsg.attach(oPart1)
   objMsg.attach(opart2)
 
@@ -84,9 +93,12 @@ def main():
     objResponse = objSMTP.send_message(objMsg)
     objSMTP.quit()
     print("Response from sendmail: {}".format(objResponse))
-    print ("Successfully sent email via {} port {} to {}".format(strServer,strPort,strRCTP))
+    print ("Successfully sent email via {} port {} to {}".format(strServer,strPort,strTo))
   except smtplib.SMTPException:
     print ("Error: unable to send email")
+
+def main():
+  SendHTMLEmail("strSubject", "strBody", "strTo", "strFrom")
 
 if __name__ == '__main__':
     main()
