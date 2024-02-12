1fedf4b95d43e3ce3a9361985ea78fb5729669a2
siggi <siggi>
2024-02-12 10:11:38 -0800

    tweak

--- ssh-config-gen.py
+++ ssh-config-gen.py
@@ -13,145 +13,152 @@ import sys
 import csv
 import platform
 
+csvDelim = ","
 iLogLevel = 5  # How much logging should be done. Level 10 is debug level, 0 is none
 if platform.system() == "Windows":
-  strProfile = os.environ["USERPROFILE"]
-  strProfile = strProfile.replace("\\","/")
+    strProfile = os.environ["USERPROFILE"]
+    strProfile = strProfile.replace("\\", "/")
 else:
-  strProfile = "~"
+    strProfile = "~"
 strConfFile = strProfile + "/.ssh/config"
 
+
 def getInput(strPrompt):
-  if sys.version_info[0] > 2:
-    return input(strPrompt)
-  else:
-    return raw_input(strPrompt)
+    if sys.version_info[0] > 2:
+        return input(strPrompt)
+    else:
+        return raw_input(strPrompt)
+
 
 def CleanExit(strCause):
-  """
-  Handles cleaning things up before unexpected exit in case of an error.
-  Things such as closing down open file handles, open database connections, etc.
-  Logs any cause given, closes everything down then terminates the script.
-  Remember to add things here that need to be cleaned up
-  Parameters:
-    Cause: simple string indicating cause of the termination, can be blank
-  Returns:
-    nothing as it terminates the script
-  """
-  if strCause != "":
-    strMsg = "{} is exiting abnormally on {} because: {}".format(
-        strScriptName, strScriptHost, strCause)
-    strTimeStamp = time.strftime("%m-%d-%Y %H:%M:%S")
-    objLogOut.write("{0} : {1}\n".format(strTimeStamp, strMsg))
-    print(strMsg)
+    """
+    Handles cleaning things up before unexpected exit in case of an error.
+    Things such as closing down open file handles, open database connections, etc.
+    Logs any cause given, closes everything down then terminates the script.
+    Remember to add things here that need to be cleaned up
+    Parameters:
+      Cause: simple string indicating cause of the termination, can be blank
+    Returns:
+      nothing as it terminates the script
+    """
+    if strCause != "":
+        strMsg = "{} is exiting abnormally on {} because: {}".format(
+            strScriptName, strScriptHost, strCause)
+        strTimeStamp = time.strftime("%m-%d-%Y %H:%M:%S")
+        objLogOut.write("{0} : {1}\n".format(strTimeStamp, strMsg))
+        print(strMsg)
 
-  if objFileOut is None:
-    objLogOut.write("Outfile not open")
-  else:
-    objFileOut.close()
-    objLogOut.write("Outfile closed")
+    if objFileOut is None:
+        objLogOut.write("Outfile not open")
+    else:
+        objFileOut.close()
+        objLogOut.write("Outfile closed")
 
-  objLogOut.close()
-  sys.exit(9)
+    objLogOut.close()
+    sys.exit(9)
+
 
 def LogEntry(strMsg, iMsgLevel, bAbort=False):
-  """
-  This handles writing all event logs into the appropriate log facilities
-  This could be a simple text log file, a database connection, etc.
-  Needs to be customized as needed
-  Parameters:
-    Message: Simple string with the event to be logged
-    iMsgLevel: How detailed is this message, debug level or general. Will be matched against Loglevel
-    Abort: Optional, defaults to false. A boolean to indicate if CleanExit should be called.
-  Returns:
-    Nothing
-  """
-  if iLogLevel > iMsgLevel:
-    strTimeStamp = time.strftime("%m-%d-%Y %H:%M:%S")
-    objLogOut.write("{0} : {1}\n".format(strTimeStamp, strMsg))
-    print(strMsg)
-  else:
+    """
+    This handles writing all event logs into the appropriate log facilities
+    This could be a simple text log file, a database connection, etc.
+    Needs to be customized as needed
+    Parameters:
+      Message: Simple string with the event to be logged
+      iMsgLevel: How detailed is this message, debug level or general. Will be matched against Loglevel
+      Abort: Optional, defaults to false. A boolean to indicate if CleanExit should be called.
+    Returns:
+      Nothing
+    """
+    if iLogLevel > iMsgLevel:
+        strTimeStamp = time.strftime("%m-%d-%Y %H:%M:%S")
+        objLogOut.write("{0} : {1}\n".format(strTimeStamp, strMsg))
+        print(strMsg)
+    else:
+        if bAbort:
+            strTimeStamp = time.strftime("%m-%d-%Y %H:%M:%S")
+            objLogOut.write("{0} : {1}\n".format(strTimeStamp, strMsg))
     if bAbort:
-      strTimeStamp = time.strftime("%m-%d-%Y %H:%M:%S")
-      objLogOut.write("{0} : {1}\n".format(strTimeStamp, strMsg))
-  if bAbort:
-    CleanExit("")
+        CleanExit("")
+
 
 def GetFileHandle(strFileName, strperm):
-  """
-  This wraps error handling around standard file open function
-  Parameters:
-    strFileName: Simple string with filename to be opened
-    strperm: single character string, usually w or r to indicate read vs write. other options such as "a" are valid too.
-  Returns:
-    File Handle object
-  """
-  dictModes = {}
-  dictModes["w"] = "writing"
-  dictModes["r"] = "reading"
-  dictModes["a"] = "appending"
-  dictModes["x"] = "opening"
+    """
+    This wraps error handling around standard file open function
+    Parameters:
+      strFileName: Simple string with filename to be opened
+      strperm: single character string, usually w or r to indicate read vs write. other options such as "a" are valid too.
+    Returns:
+      File Handle object
+    """
+    dictModes = {}
+    dictModes["w"] = "writing"
+    dictModes["r"] = "reading"
+    dictModes["a"] = "appending"
+    dictModes["x"] = "opening"
 
-  cMode = strperm[0].lower()
+    cMode = strperm[0].lower()
 
-  try:
-    objFileHndl = open(strFileName, strperm, encoding='utf8')
-    return objFileHndl
-  except PermissionError:
-    print("unable to open output file {} for {}, "
+    try:
+        objFileHndl = open(strFileName, strperm, encoding='utf8')
+        return objFileHndl
+    except PermissionError:
+        print("unable to open output file {} for {}, "
               "permission denied.".format(strFileName, dictModes[cMode]))
-    return("Permission denied")
-  except FileNotFoundError:
-    print("unable to open output file {} for {}, "
+        return ("Permission denied")
+    except FileNotFoundError:
+        print("unable to open output file {} for {}, "
               "Issue with the path".format(strFileName, dictModes[cMode]))
-    return("key not found")
+        return ("key not found")
+
 
 def createSession(dictSession):
-  """
-  This handles the actual creation of configuration item in the configuration file
-  Parameters:
-    dictSession: dictionary of the session elements: Path, Hostname, User, Cred, FW and Port
-  Returns:
-    string with error or success
-  """
+    """
+    This handles the actual creation of configuration item in the configuration file
+    Parameters:
+      dictSession: dictionary of the session elements: Path, Hostname, User, Cred, FW and Port
+    Returns:
+      string with error or success
+    """
 
-  if "Label" not in dictSession:
-    return "No Label"
-  if dictSession["Label"] == "" or dictSession["Label"] is None:
-    return "No Label"
+    if "Label" not in dictSession:
+        return "No Label"
+    if dictSession["Label"] == "" or dictSession["Label"] is None:
+        return "No Label"
 
-  if "Address" in dictSession:
-    strAddress = dictSession["Address"] or ""
-  else:
-    strAddress = ""
-  if strAddress == "":
-     return "No Address"
-  if "Username" in dictSession:
-    strUser = dictSession["Username"]  or ""
-  else:
-    strUser = ""
-  if "Jump" in dictSession:
-    strJump = dictSession["Jump"]  or ""
-  else:
-    strJump = ""
-  if "Port" in dictSession:
-    if isInt(dictSession["Port"]):
-      iPort = int(dictSession["Port"])
+    if "Address" in dictSession:
+        strAddress = dictSession["Address"] or ""
     else:
-      iPort = 22
-  else:
-    iPort = 22
+        strAddress = ""
+    if strAddress == "":
+        return "No Address"
+    if "Username" in dictSession:
+        strUser = dictSession["Username"] or ""
+    else:
+        strUser = ""
+    if "Jump" in dictSession:
+        strJump = dictSession["Jump"] or ""
+    else:
+        strJump = ""
+    if "Port" in dictSession:
+        if isInt(dictSession["Port"]):
+            iPort = int(dictSession["Port"])
+        else:
+            iPort = 22
+    else:
+        iPort = 22
 
-  strOut = "\nHost " + dictSession["Label"] + "\n"
-  strOut += "  HostName " + strAddress + "\n"
-  if strUser != "":
-    strOut += "  User " + strUser + "\n"
-  if iPort != 22:
-    strOut += "  Port " + str(iPort) + "\n"
-  if strJump != "":
-    strOut += "  ProxyCommand ssh -W %h:%p " + strJump + "\n"
-  objFileOut.write (strOut)
-  return "success!"
+    strOut = "\nHost " + dictSession["Label"] + "\n"
+    strOut += "  HostName " + strAddress + "\n"
+    if strUser != "":
+        strOut += "  User " + strUser + "\n"
+    if iPort != 22:
+        strOut += "  Port " + str(iPort) + "\n"
+    if strJump != "":
+        strOut += "  ProxyCommand ssh -W %h:%p " + strJump + "\n"
+    objFileOut.write(strOut)
+    return "success!"
+
 
 def isInt(CheckValue):
     """
@@ -170,100 +177,104 @@ def isInt(CheckValue):
         fTemp = "NULL"
     return fTemp != "NULL"
 
+
 def main():
-  global objLogOut
-  global objFileOut
-  global strScriptName
-  global strScriptHost
+    global objLogOut
+    global objFileOut
+    global strScriptName
+    global strScriptHost
 
-  strCSVName = ""
-  objFileOut = None
+    strCSVName = ""
+    objFileOut = None
 
-  ISO = time.strftime("-%Y-%m-%d-%H-%M-%S")
-  lstSysArg = sys.argv
-  iSysArgLen = len(lstSysArg)
+    ISO = time.strftime("-%Y-%m-%d-%H-%M-%S")
+    lstSysArg = sys.argv
+    iSysArgLen = len(lstSysArg)
 
-  strBaseDir = os.path.dirname(os.path.abspath(__file__))
-  strRealPath = os.path.realpath(sys.argv[0])
-  if strBaseDir == "":
-    iLoc = strRealPath.rfind("/")
-    strBaseDir = strRealPath[:iLoc]
+    strBaseDir = os.path.dirname(os.path.abspath(__file__))
+    strRealPath = os.path.realpath(sys.argv[0])
+    if strBaseDir == "":
+        iLoc = strRealPath.rfind("/")
+        strBaseDir = strRealPath[:iLoc]
 
-  strBaseDir = strBaseDir.replace("\\", "/")
+    strBaseDir = strBaseDir.replace("\\", "/")
 
-  if strBaseDir[-1:] != "/":
-      strBaseDir += "/"
+    if strBaseDir[-1:] != "/":
+        strBaseDir += "/"
 
-  strLogDir = strBaseDir + "Logs/"
+    strLogDir = strBaseDir + "Logs/"
 
-  if not os.path.exists(strLogDir):
-      print("Attempting to create log directory: {}".format(strLogDir))
-      os.makedirs(strLogDir)
+    if not os.path.exists(strLogDir):
+        print("Attempting to create log directory: {}".format(strLogDir))
+        os.makedirs(strLogDir)
 
-  strScriptName = os.path.basename(os.path.abspath(__file__))
-  iLoc = strScriptName.rfind(".")
-  strLogFile = strLogDir + strScriptName[:iLoc] + ISO + ".log"
-  strScriptHost = platform.node().upper()
-  strVersion = "{0}.{1}.{2}".format(sys.version_info[0], sys.version_info[1], sys.version_info[2])
+    strScriptName = os.path.basename(os.path.abspath(__file__))
+    iLoc = strScriptName.rfind(".")
+    strLogFile = strLogDir + strScriptName[:iLoc] + ISO + ".log"
+    strScriptHost = platform.node().upper()
+    strVersion = "{0}.{1}.{2}".format(
+        sys.version_info[0], sys.version_info[1], sys.version_info[2])
 
-  print("This is a script to create an ssh configuration file based on an input csv file. "
-        "This is running under Python Version {}".format(strVersion))
-  print("Running from: {}".format(strRealPath))
-  dtNow = time.asctime()
-  print("The time now is {}".format(dtNow))
-  print("Logs saved to {}".format(strLogFile))
+    print("This is a script to create an ssh configuration file based on an input csv file. "
+          "This is running under Python Version {}".format(strVersion))
+    print("Running from: {}".format(strRealPath))
+    dtNow = time.asctime()
+    print("The time now is {}".format(dtNow))
+    print("Logs saved to {}".format(strLogFile))
 
-  tmpObj = GetFileHandle(strLogFile,"w")
-  if isinstance(tmpObj,str):
-    CleanExit(tmpObj)
-  else:
-    objLogOut = tmpObj
-  LogEntry("Starting up",3)
+    tmpObj = GetFileHandle(strLogFile, "w")
+    if isinstance(tmpObj, str):
+        CleanExit(tmpObj)
+    else:
+        objLogOut = tmpObj
+    LogEntry("Starting up", 3)
 
-  if iSysArgLen > 1:
-    strCSVName = lstSysArg[1]
-    LogEntry("Processing input file named : {}".format(strCSVName), 4)
-  else:
+    if iSysArgLen > 1:
+        strCSVName = lstSysArg[1]
+        LogEntry("Processing input file named : {}".format(strCSVName), 4)
+    else:
+        if strCSVName == "":
+            strCSVName = getInput(
+                "Please provide full path and filename for the CSV file to be imported: ")
+
     if strCSVName == "":
-      strCSVName = getInput("Please provide full path and filename for the CSV file to be imported: ")
+        print("No filename provided unable to continue")
+        sys.exit()
 
-  if strCSVName == "":
-    print("No filename provided unable to continue")
-    sys.exit()
+    strCSVName = strCSVName.replace("\\", "/")
+    if "/" not in strCSVName:
+        strCSVName = strBaseDir + strCSVName
 
-  strCSVName = strCSVName.replace("\\", "/")
-  if "/" not in strCSVName:
-    strCSVName = strBaseDir + strCSVName
+    if os.path.isfile(strCSVName):
+        print("OK found {}".format(strCSVName))
+    else:
+        print("Can't find CSV file {}".format(strCSVName))
+        sys.exit(4)
 
-  if os.path.isfile(strCSVName):
-    print("OK found {}".format(strCSVName))
-  else:
-    print("Can't find CSV file {}".format(strCSVName))
-    sys.exit(4)
+    tmpObj = GetFileHandle(strCSVName, "r")
+    if isinstance(tmpObj, str):
+        CleanExit(tmpObj)
+    else:
+        objCSVIn = tmpObj
 
-  tmpObj = GetFileHandle(strCSVName,"r")
-  if isinstance(tmpObj,str):
-    CleanExit(tmpObj)
-  else:
-    objCSVIn = tmpObj
+    tmpObj = GetFileHandle(strConfFile, "w")
+    if isinstance(tmpObj, str):
+        CleanExit(tmpObj)
+    else:
+        objFileOut = tmpObj
+    LogEntry("{} created".format(strConfFile), 4)
 
-  tmpObj = GetFileHandle(strConfFile,"w")
-  if isinstance(tmpObj,str):
-    CleanExit(tmpObj)
-  else:
-    objFileOut = tmpObj
-  LogEntry("{} created".format(strConfFile),4)
+    objReader = csv.DictReader(objCSVIn, delimiter=csvDelim)
+    for dictTemp in objReader:
+        LogEntry("Working on {} - {} - jump: {}".format(
+            dictTemp["Label"], dictTemp["Address"], dictTemp["Jump"]), 4)
+        strRet = createSession(dictTemp)
+        LogEntry("create session returned: {}".format(strRet), 4)
 
-  objReader = csv.DictReader(objCSVIn)
-  for dictTemp in objReader:
-    LogEntry("Working on {} - {} - jump: {}".format(dictTemp["Label"],dictTemp["Address"],dictTemp["Jump"]),4)
-    strRet = createSession(dictTemp)
-    LogEntry("create session returned: {}".format(strRet),4)
-
-  # Closing thing out
-  LogEntry("Done!", 1)
-  objLogOut.close()
-  objFileOut.close()
+    # Closing thing out
+    LogEntry("Done!", 1)
+    objLogOut.close()
+    objFileOut.close()
 
 
 if __name__ == '__main__':
