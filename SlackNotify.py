53dd992530dd19abd9abe718b9e162db6c5ccb2a
Siggi Bjarnason <siggib007@users.noreply.github.com>
2025-01-28 13:37:47 +0000

    fixes

--- SlackNotify.py
+++ SlackNotify.py
@@ -12,119 +12,136 @@ import os
 import sys
 import subprocess
 try:
-    import requests
+  import requests
 except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", 'requests'])
+  subprocess.check_call([sys.executable, "-m", "pip", "install", 'requests'])
 finally:
-    import requests
+  import requests
 try:
-    import json
+  import json
 except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", 'json'])
+  subprocess.check_call([sys.executable, "-m", "pip", "install", 'json'])
 finally:
-    import json
+  import json
 # End imports
 
 
 def getInput(strPrompt):
-    if sys.version_info[0] > 2:
-        return input(strPrompt)
-    else:
-        print("Please upgrade to python 3 or greater")
-        sys.exit()
+  if sys.version_info[0] > 2:
+    return input(strPrompt)
+  else:
+    print("Please upgrade to python 3 or greater")
+    sys.exit()
 # end getInput
 
 
 def SendNotification(strMsg):
-    iTimeOut = 20  # Connection timeout in seconds
-    iMaxMSGlen = 199  # Truncate the slack message to this length
+  global strNotifyURL
 
-    strNotifyURL = "https://slack.com/api/chat.postMessage"
-    dictHeader = {}
-    dictHeader["Content-Type"] = "application/json"
-    dictHeader["Accept"] = "application/json"
-    dictHeader["Cache-Control"] = "no-cache"
-    dictHeader["Connection"] = "keep-alive"
-    dictHeader["Authorization"] = "Bearer " + strNotifyToken
+  iTimeOut = 20  # Connection timeout in seconds
+  iMaxMSGlen = 199  # Truncate the slack message to this length
 
-    dictPayload = {}
-    dictPayload["channel"] = strNotifyChannel
-    dictPayload["text"] = strMsg[:iMaxMSGlen]
+  strNotifyURL = strNotifyURL
+  dictHeader = {}
+  dictHeader["Content-Type"] = "application/json"
+  dictHeader["Accept"] = "application/json"
+  dictHeader["Cache-Control"] = "no-cache"
+  dictHeader["Connection"] = "keep-alive"
+  dictHeader["Authorization"] = "Bearer " + strNotifyToken
 
-    bStatus = False
-    WebRequest = None
-    try:
-        WebRequest = requests.post(
-            strNotifyURL, timeout=iTimeOut, json=dictPayload, headers=dictHeader)
-    except Exception as err:
-        return "FAIL. Issue with sending notifications. {}".format(err)
-    if WebRequest is not None:
-        if isinstance(WebRequest, requests.models.Response) == False:
-            return "FAIL. Response is unknown type"
-        else:
-            dictResponse = json.loads(WebRequest.text)
-            if isinstance(dictResponse, dict):
-                if "ok" in dictResponse:
-                    bStatus = dictResponse["ok"]
-                    if bStatus:
-                        return "OK. Successfully sent the following slack notification:\n{} ".format(strMsg[:iMaxMSGlen])
-                    else:
-                        return "FAIL. Failed to send slack message:{} ".format(dictResponse["error"])
-                else:
-                    return "FAIL. Slack unexpected response from slack: {}".format(WebRequest.text)
-            else:
-                return "FAIL. Failed to load response into a dictionary, here is what came back: {}".format(WebRequest.text)
+  dictPayload = {}
+  dictPayload["channel"] = strNotifyChannel
+  dictPayload["text"] = strMsg[:iMaxMSGlen]
+
+  bStatus = False
+  WebRequest = None
+  try:
+    WebRequest = requests.post(
+      strNotifyURL, timeout=iTimeOut, json=dictPayload, headers=dictHeader)
+  except Exception as err:
+    return "FAIL. Issue with sending notifications. {}".format(err)
+  if WebRequest is not None:
+    if isinstance(WebRequest, requests.models.Response) == False:
+      return "FAIL. Response is unknown type"
     else:
-        return "FAIL. WebRequest not defined, implies that the call was not attempted for some reason"
+      dictResponse = json.loads(WebRequest.text)
+      if isinstance(dictResponse, dict):
+        if "ok" in dictResponse:
+            bStatus = dictResponse["ok"]
+            if bStatus:
+              return "OK. Successfully sent the following slack notification:\n{} ".format(strMsg[:iMaxMSGlen])
+            else:
+              return "FAIL. Failed to send slack message:{} ".format(dictResponse["error"])
+        else:
+            return "FAIL. Slack unexpected response from slack: {}".format(WebRequest.text)
+      else:
+        return "FAIL. Failed to load response into a dictionary, here is what came back: {}".format(WebRequest.text)
+  else:
+    return "FAIL. WebRequest not defined, implies that the call was not attempted for some reason"
 
 
 def initvars():
-    global strNotifyChannel
-    global strNotifyToken
+  global strNotifyChannel
+  global strNotifyToken
+  global strNotifyURL
+  global strNotifyEnable
 
-    if os.getenv("NOTIFYCHANNEL") != "" and os.getenv("NOTIFYCHANNEL") is not None:
-        strNotifyChannel = os.getenv("NOTIFYCHANNEL")
-    else:
-        strNotifyChannel = None
-        print("No Notify Channel provided")
+  if os.getenv("NOTIFYENABLE") != "" and os.getenv("NOTIFYENABLE") is not None:
+    strNotifyEnable = os.getenv("NOTIFYENABLE")
+  else:
+    strNotifyEnable = None
+    print("No Notify enable provided")
 
-    if os.getenv("NOTIFYTOKEN") != "" and os.getenv("NOTIFYTOKEN") is not None:
-        strNotifyToken = os.getenv("NOTIFYTOKEN")
-    else:
-        strNotifyToken = None
-        print("No token provided")
+  if os.getenv("NOTIFYURL") != "" and os.getenv("NOTIFYURL") is not None:
+    strNotifyURL = os.getenv("NOTIFYURL")
+  else:
+    strNotifyURL = None
+    print("No Notify URL provided")
 
-    if strNotifyToken is None or strNotifyChannel is None:
-        return False
+  if os.getenv("NOTIFYCHANNEL") != "" and os.getenv("NOTIFYCHANNEL") is not None:
+    strNotifyChannel = os.getenv("NOTIFYCHANNEL")
+  else:
+    strNotifyChannel = None
+    print("No Notify Channel provided")
+
+  if os.getenv("NOTIFYTOKEN") != "" and os.getenv("NOTIFYTOKEN") is not None:
+    strNotifyToken = os.getenv("NOTIFYTOKEN")
+  else:
+    strNotifyToken = None
+    print("No token provided")
+
+  if strNotifyToken is None or strNotifyChannel is None or strNotifyURL is None:
+    return False
+  else:
+    if strNotifyEnable == "false":
+      return False
     else:
-        return True
+      return True
 
 
 def main():
+  bNotifyEnable = initvars()
+  if not bNotifyEnable:
+    print("notifications not enable, can't do anything")
+    sys.exit(5)
 
-    bNotifyEnable = initvars()
-    if not bNotifyEnable:
-        print("notifications not enable, can't do anything")
-        sys.exit(5)
+  if len(sys.argv) > 1:
+    strMsg = sys.argv[1]
+  else:
+    strMsg = ""
 
-    if len(sys.argv) > 1:
-        strMsg = sys.argv[1]
-    else:
-        strMsg = ""
+  if strMsg == "":
+    strMsg = getInput("Please provide a message to send: ")
 
-    if strMsg == "":
-        strMsg = getInput(
-            "Please provide a message to send: ")
+  if strMsg == "":
+    print("No message provided, nothing to do")
+    sys.exit(2)
 
-    if strMsg == "":
-        print("No message provided, nothing to do")
-        sys.exit(2)
-
-    if bNotifyEnable:
-        print(SendNotification(strMsg))
-    else:
-        print("unable to send notifications, not enabled")
+  if bNotifyEnable:
+    print(SendNotification(strMsg))
+  else:
+    print("unable to send notifications, not enabled")
 
 
 if __name__ == '__main__':
-    main()
+  main()
