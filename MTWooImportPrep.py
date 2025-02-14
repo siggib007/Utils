6ce12a8109e5f6329b2061c56e8378bbec106f29
Siggi Bjarnason <siggib007@users.noreply.github.com>
2025-02-14 20:46:11 +0200

    getimage fix, hires only

--- MTWooImportPrep.py
+++ MTWooImportPrep.py
@@ -168,8 +168,8 @@ def GetProductDetails(strURL):
   objSoup = BeautifulSoup(strHTML,features="html5lib")
   if iVerbose > 1:
     LogEntry("Fetched URL and parsed into a beautiful Soup, response length is {}".format(len(strHTML)))
-  for objLink in objSoup.findAll("img"):
-    strImg = objLink.get("src")
+  for objLink in objSoup.findAll("a"):
+    strImg = objLink.get("href")
     if strImg is not None and strImg[:4] == "http" and strImg[-4:] == ".png":
       strRet += strImg + ","
       if iVerbose > 2:
